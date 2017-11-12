6949c3e10f042b2cc4d73814b452a4a0974a6c45
yuedaregone <810566249@qq.com>
2017-11-12 20:17:41 +0800

    waste time

--- Read/src/AppDelegate.cpp
+++ Read/src/AppDelegate.cpp
@@ -14,7 +14,7 @@ void  AppDelegate::appLaunchFinish()
 	MScene* Hello = HelloWorld::scene();
 	g_mDirector->runWithScene(Hello);
 
-	//g_mDirector->getGraphics()->setOpacity(0);
-	//MAction* act = MFadeTo::createFadeTo(255, 2.0f);
-	//g_mDirector->getGraphics()->runAction(act);
+	g_mDirector->getGraphics()->setOpacity(0);
+	MAction* act = MFadeTo::createFadeTo(255, 0.5f);
+	g_mDirector->getGraphics()->runAction(act);
 }
